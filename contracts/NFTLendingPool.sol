c16e1e9f56071f03030b085af8ba13816fc2bb12
Alex Bazhanau <bazhanau@outlook.com>
2022-06-25 11:34:29 -0400

    Initial commit

---
+++ contracts/NFTLendingPool.sol
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.7.0;
+
+import "./PoolStorage.sol";
+
+
+contract NFTLendingPool is CErc20Interface {
+
+	/**
+     * @notice Sender supplies assets into the market and receives cTokens in exchange
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param mintAmount The amount of the underlying asset to supply
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function mint(uint mintAmount) external returns (uint) {
+        mintInternal(mintAmount);
+        return NO_ERROR;
+    }
+
+	/**
+     * @notice Sender redeems cTokens in exchange for the underlying asset
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param redeemTokens The number of cTokens to redeem into underlying
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function redeem(uint redeemTokens) external returns (uint) {
+        redeemInternal(redeemTokens);
+        return NO_ERROR;
+    }
+
+	function depositNFT(address nft, uint id) external returns (uint) {
+		
+	}
+
+	function withdrawNFT(address nft, uint id) external returns (uint) {
+		
+	}
+
+	/**
+      * @notice Sender borrows assets from the protocol to their own address
+      * @param borrowAmount The amount of the underlying asset to borrow
+      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+      */
+    function borrow(uint borrowAmount) external returns (uint) {
+        borrowInternal(borrowAmount);
+        return NO_ERROR;
+    }
+
+    /**
+     * @notice Sender repays their own borrow
+     * @param repayAmount The amount to repay, or -1 for the full outstanding amount
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function repayBorrow(uint repayAmount) external returns (uint) {
+        repayBorrowInternal(repayAmount);
+        return NO_ERROR;
+    }
+
+	/**
+     * @notice The sender liquidates the borrowers collateral.
+     *  The collateral seized is transferred to the liquidator.
+     * @param borrower The borrower of this cToken to be liquidated
+     * @param repayAmount The amount of the underlying borrowed asset to repay
+     * @param cTokenCollateral The market in which to seize collateral from the borrower
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function liquidateBorrow(address borrower, uint repayAmount, CTokenInterface cTokenCollateral) external returns (uint) {
+        liquidateBorrowInternal(borrower, repayAmount, cTokenCollateral);
+        return NO_ERROR;
+    }
+
+	function transferTokens(address spender, address src, address dst, uint tokens) internal returns (uint) {
+        /* Fail if transfer not allowed */
+        uint allowed = comptroller.transferAllowed(address(this), src, dst, tokens);
+        if (allowed != 0) {
+            revert TransferComptrollerRejection(allowed);
+        }
+
+        /* Do not allow self-transfers */
+        if (src == dst) {
+            revert TransferNotAllowed();
+        }
+
+        /* Get the allowance, infinite for the account owner */
+        uint startingAllowance = 0;
+        if (spender == src) {
+            startingAllowance = type(uint).max;
+        } else {
+            startingAllowance = transferAllowances[src][spender];
+        }
+
+        /* Do the calculations, checking for {under,over}flow */
+        uint allowanceNew = startingAllowance - tokens;
+        uint srcTokensNew = accountTokens[src] - tokens;
+        uint dstTokensNew = accountTokens[dst] + tokens;
+
+        /////////////////////////
+        // EFFECTS & INTERACTIONS
+        // (No safe failures beyond this point)
+
+        accountTokens[src] = srcTokensNew;
+        accountTokens[dst] = dstTokensNew;
+
+        /* Eat some of the allowance (if necessary) */
+        if (startingAllowance != type(uint).max) {
+            transferAllowances[src][spender] = allowanceNew;
+        }
+
+        /* We emit a Transfer event */
+        emit Transfer(src, dst, tokens);
+
+        // unused function
+        // comptroller.transferVerify(address(this), src, dst, tokens);
+
+        return NO_ERROR;
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transfer(address dst, uint256 amount) override external nonReentrant returns (bool) {
+        return transferTokens(msg.sender, msg.sender, dst, amount) == NO_ERROR;
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `src` to `dst`
+     * @param src The address of the source account
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transferFrom(address src, address dst, uint256 amount) override external nonReentrant returns (bool) {
+        return transferTokens(msg.sender, src, dst, amount) == NO_ERROR;
+    }
+
+    /**
+     * @notice Approve `spender` to transfer up to `amount` from `src`
+     * @dev This will overwrite the approval amount for `spender`
+     *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (uint256.max means infinite)
+     * @return Whether or not the approval succeeded
+     */
+    function approve(address spender, uint256 amount) override external returns (bool) {
+        address src = msg.sender;
+        transferAllowances[src][spender] = amount;
+        emit Approval(src, spender, amount);
+        return true;
+    }
+
+    /**
+     * @notice Get the current allowance from `owner` for `spender`
+     * @param owner The address of the account which owns the tokens to be spent
+     * @param spender The address of the account which may transfer tokens
+     * @return The number of tokens allowed to be spent (-1 means infinite)
+     */
+    function allowance(address owner, address spender) override external view returns (uint256) {
+        return transferAllowances[owner][spender];
+    }
+
+    /**
+     * @notice Get the token balance of the `owner`
+     * @param owner The address of the account to query
+     * @return The number of tokens owned by `owner`
+     */
+    function balanceOf(address owner) override external view returns (uint256) {
+        return accountTokens[owner];
+    }
+
+    /**
+     * @notice Get the underlying balance of the `owner`
+     * @dev This also accrues interest in a transaction
+     * @param owner The address of the account to query
+     * @return The amount of underlying owned by `owner`
+     */
+    function balanceOfUnderlying(address owner) override external returns (uint) {
+        Exp memory exchangeRate = Exp({mantissa: exchangeRateCurrent()});
+        return mul_ScalarTruncate(exchangeRate, accountTokens[owner]);
+    }
+}
