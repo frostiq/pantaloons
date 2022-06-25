b2d4a8725a0d5a0a05f42c87a3fba232ed523c3f
Alex Bazhanau <bazhanau@outlook.com>
2022-06-25 15:16:14 -0400

    mint and redeem implemented

--- contracts/NFTLendingPool.sol
+++ contracts/NFTLendingPool.sol
@@ -1,17 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import 'abdk-libraries-solidity/ABDKMath64x64.sol';
 import "./INFTLendingPool.sol";
 
 contract NFTLendingPool is INFTLendingPool, ERC20 {
+    using ABDKMath64x64 for int128; 
+
+    IERC20 public underlyingToken;
 
     constructor() ERC20("Pantaloons USDC", "pUSDC") {
     }
 
-    function mint(uint256 mintAmount) external {}
+    function mint(uint256 amount) external {
+        int128 exchangeRate = exchangeRateStored();
 
-    function redeem(uint256 redeemTokens) external {}
+        underlyingToken.transferFrom(msg.sender, address(this), amount);
+        uint256 mintTokens = exchangeRate.mulu(amount);
+
+        _mint(msg.sender, mintTokens);
+    }
+
+    function redeem(uint256 redeemTokens) external {
+        int128 exchangeRate = exchangeRateStored();
+
+        _burn(msg.sender, redeemTokens);
+
+        uint256 withdrawAmount = ABDKMath64x64.divu(1, 1).div(exchangeRate).mulu(redeemTokens);
+        underlyingToken.transfer(msg.sender, withdrawAmount);
+    }
 
     function borrow(uint256 borrowAmount) external {}
 
@@ -24,4 +43,9 @@ contract NFTLendingPool is INFTLendingPool, ERC20 {
     function liquidateBorrow(IERC721 nft, uint256 id) external {}
 
     function appraise(IERC721 nft, uint256 appraisal) external {}
+
+    function exchangeRateStored() public returns (int128){
+        int128 half = ABDKMath64x64.divu(1, 2);
+        return half;
+    }
 }
