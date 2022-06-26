4ea91248aedbfafd996a4bb676f4d25560680766
abindroo-bc <105650979+abindroo-bc@users.noreply.github.com>
2022-06-26 06:51:31 -0400

    Removed RequestAppraisal

--- contracts/Appraiser.sol
+++ contracts/Appraiser.sol
@@ -20,19 +20,11 @@ contract Appraiser is Ownable {
     // Mapping of NFT collection addresses to their NFT appraisal
     mapping (address => NFTAppraisal) public appraisals;
 
-    event AppraisalRequested(address indexed nftAddress, uint16 defaultProbability, uint256 loanTime, address requesterAddress);
     event AppraisalSubmitted(address indexed nftAddress, uint256 volatility, uint256 lastPrice, uint256 recommendedLoanAmount, uint256 lastUpdateTimestamp);
     
     // sets owner to msg.sender by default
     constructor() {
         console.log("Welcome to Appraisal contract");
-    }
-
-    // Request for an appraisal by emitting an event
-    function requestAppraisal(address nftAddress, uint16 defaultProbability, uint256 loanTime) public payable {
-        require(msg.value == appraisalFee, "Please pay the appraisal fee");
-        totalFeesCollected += msg.value;
-        emit AppraisalRequested(nftAddress, defaultProbability, loanTime, msg.sender);
     }
 
     // Only owner contract can set the appraisal, updating the NFTAppraisal information
